9158ddce3654eb22f954f293f866053269848fd4
Renanpacheco <prenan86@gmail.com>
2025-01-09 18:28:40 -0300

    add input for register of user

---
+++ src/components/form/Input.js
@@ -0,0 +1,18 @@
+import styles from "./Input.module.css"
+
+function Input({ type, text, name, placeholder, handleOnchange, value, multiple}){
+    return(
+        <div>
+            <label htmlFor={name}>{text}:</label>
+            <input type={type} 
+            name={name} 
+            id={name} 
+            placeholder= {placeholder} 
+            onChange={handleOnchange} 
+            value={value} 
+            {...(multiple ? {multiple} : '')}/>
+        </div>
+    )
+}
+
+export default Input
--- src/components/pages/Auth/Register.js
+++ src/components/pages/Auth/Register.js
@@ -1,7 +1,18 @@
+import Input from "../../form/Input.js"
+
 function Register(){
+    function handlechange(e){}
     return (
         <section>
             <h1>Register</h1>
+            <form>
+                <Input text="name" name="name" type="text" placeholder="Enter your name" handleOnchange={handlechange} />
+                <Input text="phone" name="phone" type="text" placeholder="Enter your phone" handleOnchange={handlechange} />
+                <Input text="E-mail" name="email" type="email" placeholder="Enter your e-mail" handleOnchange={handlechange} />
+                <Input text="password" name="password" type="password" placeholder="Enter your password" handleOnchange={handlechange} />
+                <Input text="confirm password" name="confirmpassword" type="password" placeholder="confirm your password" handleOnchange={handlechange} />
+                <input type="submit" value="register" />
+            </form>
         </section>
     )
 
