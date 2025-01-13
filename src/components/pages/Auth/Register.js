568ad2dc4ad589ca7286e44fa1f0ffdc3ae627a9
Renanpacheco <prenan86@gmail.com>
2025-01-13 18:56:06 -0300

    fixed problem with axior and confirm password

--- src/components/pages/Auth/Register.js
+++ src/components/pages/Auth/Register.js
@@ -22,11 +22,11 @@ function Register(){
         <section className={styles.form_container}>
             <h1>Register</h1>
             <form onSubmit={handleSubmit}>
-                <Input text="name" name="name" type="text" placeholder="Enter your name" handleOnchange={handlechange} />
-                <Input text="phone" name="phone" type="text" placeholder="Enter your phone" handleOnchange={handlechange} />
-                <Input text="E-mail" name="email" type="email" placeholder="Enter your e-mail" handleOnchange={handlechange} />
-                <Input text="password" name="password" type="password" placeholder="Enter your password" handleOnchange={handlechange} />
-                <Input text="confirm password" name="confirmpassword" type="password" placeholder="confirm your password" handleOnchange={handlechange} />
+                <Input type="text" text="name" name="name" placeholder="Enter your name" handleOnchange={handlechange} />
+                <Input type="text" text="phone" name="phone" placeholder="Enter your phone" handleOnchange={handlechange} />
+                <Input type="email" text="E-mail" name="email" placeholder="Enter your e-mail" handleOnchange={handlechange} />
+                <Input type="password" text="password" name="password"  placeholder="Enter your password" handleOnchange={handlechange} />
+                <Input type="password" text="confirm" name="confirm" placeholder="confirm your password" handleOnchange={handlechange} />
                 <input type="submit" value="register" />
             </form>
             <p>Already have a account? <Link to="/login">Click here</Link></p>
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,14 +1,14 @@
-//import api from "../utils/api"
+import api from "../utils/api"
 /*import { useState, useEffect } from "react"
 import { useHistory} from 'react-router-dom'*/
-import axios from "axios";
+//import axios from "axios";
 
 export default function useAuth(){
     
     async function register(user){
 
         try {
-            const data = await axios.post('http://localhost:5000/users/register', user).then((response)=>{
+            const data = await api.post('users/register', user).then((response)=>{
                 return response.data
             })
             console.log(data)
