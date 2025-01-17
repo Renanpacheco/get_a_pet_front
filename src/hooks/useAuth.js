e2a958a677c4bb54a16f4b153dd78c24459025c6
Renanpacheco <prenan86@gmail.com>
2025-01-17 11:10:13 -0300

    authenticated user after login

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,22 @@
 import api from "../utils/api"
 import useFlashMessage from "./useFlashMessage"
-/*import { useState, useEffect } from "react"
-import { useHistory} from 'react-router-dom'*/
+import { useState, useEffect } from "react"
+import { useNavigate } from 'react-router-dom'
 
 
 export default function useAuth(){
     const { setFlashMessage} = useFlashMessage()
+    const [authenticated, setAuthenticated] = useState(false)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if (token) {
+            api.defaults.headers.Autorization =`Bearer ${JSON.parse(token)}`
+            setAuthenticated(true)
+        }
+    }, [])
     
     async function register(user){
         
@@ -16,7 +27,8 @@ export default function useAuth(){
             const data = await api.post('users/register', user).then((response)=>{
                 return response.data
             })
-            console.log(data)
+            //console.log(data)
+            await authUser(data);
         } catch (error) {
             msgText = error.response.data.message
             msgType = "error";
@@ -24,5 +36,14 @@ export default function useAuth(){
         }
         setFlashMessage(msgText,msgType)
     }
-    return {register}
+    
+
+    async function authUser(data){
+        setAuthenticated(true)
+        localStorage.setItem('token', JSON.stringify(data.token))
+        navigate('/')
+
+    }
+
+    return {authenticated, register}
 }
